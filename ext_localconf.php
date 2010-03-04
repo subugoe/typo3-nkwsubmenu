78342c2de625e36562e5b8f0491c735e33a220cc
windisch <windisch@sub.uni-goettingen.de>
2010-03-04 07:40:47 +0000

    ...
    git-svn-id: https://develop.sub.uni-goettingen.de/repos/w3d/branches/dev/typo3conf/ext/nkwsubmenu@269 f004c202-0a6e-0410-85e4-8d7485fa9ffe

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,5 @@
 <?php
-if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
 t3lib_extMgm::addTypoScript($_EXTKEY,'editorcfg','tt_content.CSS_editor.ch.tx_nkwsubmenu_pi1 = < plugin.tx_nkwsubmenu_pi1.CSS_editor',43);
 t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_nkwsubmenu_pi1.php','_pi1','list_type',1);
@@ -9,11 +9,4 @@ t3lib_extMgm::addPItoST43($_EXTKEY,'pi2/class.tx_nkwsubmenu_pi2.php','_pi2','lis
 
 t3lib_extMgm::addTypoScript($_EXTKEY,'editorcfg','tt_content.CSS_editor.ch.tx_nkwsubmenu_pi3 = < plugin.tx_nkwsubmenu_pi3.CSS_editor',43);
 t3lib_extMgm::addPItoST43($_EXTKEY,'pi3/class.tx_nkwsubmenu_pi3.php','_pi3','list_type',1);
-
-/*
-function user_checkHideInfobox($cmd)
-{	
-}
-*/
-
 ?>
