4af65e32404da98cbbf8b0adf268b14215402ea8
windisch <windisch@sub.uni-goettingen.de>
2010-09-02 08:44:57 +0000

    git-svn-id: https://develop.sub.uni-goettingen.de/repos/w3d/branches/dev/typo3conf/ext/nkwsubmenu@614 f004c202-0a6e-0410-85e4-8d7485fa9ffe

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,26 +1,15 @@
 <?php
-/***************************************************************
-*  Copyright notice
-*
-*  (c) 2010 Nils K. Windisch <windisch@sub.uni-goettingen.de>
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+
+########################################################################
+# Extension Manager/Repository config file for ext "nkwsubmenu".
+#
+# Auto generated 01-09-2010 17:21
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'SUB Menu',
 	'description' => 'Lorem ipsum dolor sit amet',
@@ -40,14 +29,19 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'Goettingen State and University Library, Germany http://www.sub.uni-goettingen,de',
-	'version' => '0.0.34',
+	'version' => '0.0.35',
 	'constraints' => array(
 		'depends' => array(
 			'nkwlib' => '',
 		),
-		'conflicts' => array(),
-		'suggests' => array(),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
 	),
-	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"83d3";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"d54d";s:14:"ext_tables.php";s:4:"3196";s:14:"ext_tables.sql";s:4:"36fd";s:16:"locallang_db.xml";s:4:"4d75";s:19:"doc/wizard_form.dat";s:4:"9a97";s:20:"doc/wizard_form.html";s:4:"e140";s:31:"pi1/class.tx_nkwsubmenu_pi1.php";s:4:"e3c1";s:16:"pi1/flexform.xml";s:4:"d41d";s:17:"pi1/locallang.xml";s:4:"5ecd";s:24:"pi1/static/editorcfg.txt";s:4:"b8f9";}',
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"4a95";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"c8ca";s:17:"ext_localconf.php";s:4:"fc0f";s:14:"ext_tables.php";s:4:"93b0";s:14:"ext_tables.sql";s:4:"7a72";s:16:"locallang_db.xml";s:4:"ab6b";s:19:"doc/wizard_form.dat";s:4:"9a97";s:20:"doc/wizard_form.html";s:4:"e140";s:31:"pi1/class.tx_nkwsubmenu_pi1.php";s:4:"1ca7";s:16:"pi1/flexform.xml";s:4:"d41d";s:17:"pi1/locallang.xml";s:4:"5ecd";s:31:"pi2/class.tx_nkwsubmenu_pi2.php";s:4:"d1ac";s:17:"pi2/locallang.xml";s:4:"c1d7";s:31:"pi3/class.tx_nkwsubmenu_pi3.php";s:4:"f9f9";s:17:"pi3/locallang.xml";s:4:"08f8";}',
+	'suggests' => array(
+	),
 );
+
 ?>
